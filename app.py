1029a5a1b5f20a4d45598e971bd9b24766dd6f26
cagdas <cagdasyanar@outlook.com>
2019-07-10 10:43:31 +0300

    add some file

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask,request,url_for,redirect,render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://cagdas:123456@localhost:5432/postgres"
+
+db = SQLAlchemy(app)
+class Note(db.Model): #note diye model tanımladım
+    __tablename__ = 'simplenotetable' # bunun tablodaki ismi note
+    id = db.Column(db.Integer, primary_key=True)
+    header = db.Column(db.String)
+    note = db.Column(db.String)
+    isprivate = db.Column(db.Boolean, server_default='f', default=False, nullable=False)
+    password = db.Column(db.String)
+    #initi
+    def __init__(self,header, note,password,isprivate):
+        self.header = header
+        self.note = note
+        self.password = password
+        self.isprivate = isprivate
+#note,isprivate,password
+   # def __repr__(self):
+    #    return f"Note('{self.header}','{self.note}','{self.isprivate}','{self.password}')"
+
+
+
+@app.route('/', methods = ['POST','GET'] )
+def index():
+    if request.method == 'POST':
+        for req in request.form:
+            if req == "isprivate":
+                privateControl = True
+                break
+            else :
+                privateControl = False
+        if privateControl == True:
+            data = Note(header=request.form['header'], note=request.form['note'], password=request.form['password'], isprivate= True)
+        else:
+            data = Note(header = request.form['header'], note = request.form['note'], password = "", isprivate= False)
+
+        db.session.add(data) # ekleme
+        db.session.commit() # commit et
+        return redirect(url_for('index'))
+    else:
+        dataQuery = Note.query.all()
+        reverseData = []
+        for dataItem in dataQuery:
+            reverseData.append(dataItem)
+        reverseData.reverse()
+        id = 0
+        selectData = 0
+        return render_template("index.html", dataQuery= reverseData, id = id, selectData = selectData)
+
+@app.route('/<string:id>/')
+def detail(id):
+    dataQuery = Note.query.all()
+    reverseData = []
+    for dataItem in dataQuery:
+        reverseData.append(dataItem)
+    reverseData.reverse()
+    selectData = Note.query.filter_by(id=id).first()
+    return render_template('index.html', id=id ,dataQuery= reverseData, selectData = selectData)
+if __name__ == '__main__':
+    app.run()
+
+#TODO: LinkVer tik
+#TODO: Private
+#TODO: wow.JS
+#TODO: Tarihi Düzenle
+#TODO: Kodu düzenle
+#TODO: Deploy
---
+++ static/main.js
@@ -0,0 +1,53 @@
+
+
+function onHover()
+{
+    $(".addNoteImg").attr('src', '../static/compose2.png');
+}
+
+function offHover()
+{
+    $(".addNoteImg").attr('src', '../static/compose.png');
+}
+
+function cardOver(element) {
+    element.classList.add("cardHover")
+}
+function cardOut(element) {
+    element.classList.remove("cardHover")
+}
+
+document.getElementById("passwordEye").addEventListener("click", function(){
+   var x = document.getElementById("showHideIn").getAttribute("type");
+   console.log(x);
+  if( x === "password") {
+      document.getElementById("showHideIn").setAttribute("type", "text");
+      document.getElementById("passwordEye").classList.remove("fa-eye")
+      document.getElementById("passwordEye").classList.add("fa-eye-slash")
+  }
+else if (x== "text"){
+   document.getElementById("showHideIn").setAttribute("type", "password");
+      document.getElementById("passwordEye").classList.remove("fa-eye-slash")
+      document.getElementById("passwordEye").classList.add("fa-eye")
+  }
+
+});
+
+document.getElementById("formSwitch").addEventListener("click", function() {
+    let isPrivate = document.getElementById("formSwitch").getAttribute("placeholder");
+    if (isPrivate === "yes") {
+        document.getElementById("formSwitch").setAttribute("placeholder", "no");
+        document.getElementById("showHideIn").value = "";
+        document.getElementById("showHideIn").disabled = true;
+    } else if (isPrivate === "no") {
+        document.getElementById("formSwitch").setAttribute("placeholder", "yes");
+        document.getElementById("showHideIn").disabled = false;
+    }
+});
+
+document.getElementById("search_icon").addEventListener('click',function () {
+    let input = document.getElementById("search_input").value;
+    document.getElementById("search_icon").setAttribute("href",input);
+
+});
+
