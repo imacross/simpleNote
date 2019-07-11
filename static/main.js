bfee4816fd246eafa886b4f92c03bce77245bb25
cagdas <cagdasyanar@outlook.com>
2019-07-11 17:12:41 +0300

    fixed note problem

--- static/main.js
+++ static/main.js
@@ -15,29 +15,36 @@ function cardOut(element) {
 }
 
 document.getElementById("passwordEye").addEventListener("click", function () {
-    var x = document.getElementById("showHideIn").getAttribute("type");
+    showHide = document.getElementById("showHideIn");
+    icon = document.getElementById("passwordEye").classList;
+
+    var x = showHide.getAttribute("type");
     console.log(x);
     if (x === "password") {
-        document.getElementById("showHideIn").setAttribute("type", "text");
-        document.getElementById("passwordEye").classList.remove("fa-eye")
-        document.getElementById("passwordEye").classList.add("fa-eye-slash")
+        showHide.setAttribute("type", "text");
+        icon.remove("fa-eye");
+        icon.add("fa-eye-slash");
     } else if (x == "text") {
-        document.getElementById("showHideIn").setAttribute("type", "password");
-        document.getElementById("passwordEye").classList.remove("fa-eye-slash")
-        document.getElementById("passwordEye").classList.add("fa-eye")
+        showHide.setAttribute("type", "password");
+        icon.remove("fa-eye-slash");
+        icon.add("fa-eye");
     }
 
 });
 
 document.getElementById("formSwitch").addEventListener("click", function () {
+    showHide = document.getElementById("showHideIn");
+    icon = document.getElementById("passwordEye").classList;
     let isPrivate = document.getElementById("formSwitch").getAttribute("placeholder");
     if (isPrivate === "yes") {
+
         document.getElementById("formSwitch").setAttribute("placeholder", "no");
-        document.getElementById("showHideIn").value = "";
-        document.getElementById("showHideIn").disabled = true;
+        showHide.value = "";
+        showHide.disabled = true;
+        showHide.disabled = true;
     } else if (isPrivate === "no") {
         document.getElementById("formSwitch").setAttribute("placeholder", "yes");
-        document.getElementById("showHideIn").disabled = false;
+        showHide.disabled = false;
     }
 });
 
@@ -47,29 +54,56 @@ document.getElementById("search_icon").addEventListener('click', function () {
 });
 
 $(document).ready(() => {
-   $('#show').on('click',(e) =>{
-       $.ajax({
-           data:{
-               sPassword : $('#privateInputPassword').val(),
-               sId : $('#sId').attr("placeholder")
-           },
-           type: 'POST',
-           url: '/passwordcontrol'
-       })
-           .done((data) => {
-               if (data.error){
-                   console.log(data.error)
+    $("#kapgit").click(function (event) {
+        event.preventDefault();
+        inputData = {
+            header: $('#formHeader').val(),
+            note: $('#formText').val(),
+            isPrivate: $('#formSwitch').attr('placeholder'),
+            password: $('#showHideIn').val()
+
+        };
+        if(inputData.header == ""){
+            $("#n1").css("display","inline-block");
+        }
+        else if(inputData.note == ""){
+            $("#n1").css("display","none");
+            $("#n2").css("display","inline-block");
+        }
+        else if(inputData.isPrivate === "yes" && inputData.password == "" ){
+            $("#n1").css("display","none");
+            $("#n2").css("display","none");
+            $("#n3").css("display","inline-block");
+        }
+        else{
+            $("#n1,#n2,#n3").css("display","none");
+            $("#addNoteForm").submit();
+        }
+        console.log(inputData)
+    });
+
+    $('#show').on('click', (e) => {
+        $.ajax({
+            data: {
+                sPassword: $('#privateInputPassword').val(),
+                sId: $('#sId').attr("placeholder")
+            },
+            type: 'POST',
+            url: '/passwordcontrol'
+        })
+            .done((data) => {
+                if (data.error) {
+                    console.log(data.error)
                     $('.wrongPassword').css("display", "block");
-               }
-               else{
+                } else {
                     $('#privateHeader').text(data.header);
                     $('#privateContent').text(data.note);
                     $('#privateModal').modal('hide');
                     $('#privateDetailModal').modal('show');
                     console.log(data.sPassword)
-               }
-           })
-   });
+                }
+            })
+    });
 });
 
 
